a3014fbdd26f97453c86ef75474ab4e9af92e8ec
Mathieu Lecarme <mlecarme@bearstech.com>
2021-08-11 19:42:02 +0200

    Dead code.

--- cli/server/main.go
+++ cli/server/main.go
@@ -4,45 +4,11 @@ import (
 	"encoding/gob"
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"syscall"
 
 	"github.com/factorysh/chownme/server"
 )
-
-func echoServer(c net.Conn) {
-	for {
-		buf := make([]byte, 512)
-		oob2 := make([]byte, 10*24)
-		nr, oobn2, flags, _, err := c.(*net.UnixConn).ReadMsgUnix(buf, oob2)
-		if err != nil {
-			return
-		}
-
-		fmt.Println("flags", flags)
-
-		oob2 = oob2[:oobn2]
-		scm, err := syscall.ParseSocketControlMessage(oob2)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("scm", scm)
-		newUcred, err := syscall.ParseUnixCredentials(&scm[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("ucred", newUcred)
-
-		data := buf[0:nr]
-		println("Server got:", string(data))
-		_, err = c.Write(data)
-		if err != nil {
-			log.Fatal("Write: ", err)
-		}
-	}
-}
 
 func main() {
 	listen := os.Getenv("LISTEN")
